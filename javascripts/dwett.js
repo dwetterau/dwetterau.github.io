123fb69b8fd73eaaa9d5be3053d23a3769f11797
David Wetterau <david.wetterau@gmail.com>
2014-11-17 11:53:43 -0600

    css cleanup and transition fade

--- javascripts/dwett.js
+++ javascripts/dwett.js
@@ -7,6 +7,19 @@ $('#about_link, #return_link').on('click', function(event) {
   return false;
 });
 
+$(document).ready(function() {
+  $("body").css("display", "none");
+  $("body").fadeIn(250);
+
+  $("a.transition").click(function(event) {
+    event.preventDefault();
+    new_url = this.href;
+    $("body").fadeOut(250, function() {
+        window.location = new_url
+    });
+  });
+});
+
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
